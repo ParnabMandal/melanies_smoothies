7a4e4c4c0f0b71d53a50938fabc2b577faff1288
Parnab Mandal <62804796+ParnabMandal@users.noreply.github.com>
2025-11-03 08:25:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize  Smoothie! :cup_with_straw:")
 st.write(
@@ -28,6 +28,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen +' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -40,9 +43,4 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered!', icon="✅")
 
-#new section to display smoothiefroot nutrition information
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
